83e3eca540eadd200990159866155c6ba6ed0861
carolinarecio <100321415+carolinarecio@users.noreply.github.com>
2022-02-23 17:33:25 -0600

    Add files via upload

---
+++ citadental.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int opcion,edad[3];
+	int salir=1; 
+	char nompac[30][3], nomtrat[30][3], desc[100][3];
+	do {
+
+// hay una funcion que se llama getline que sirvepara los espacios y existen diferetnes sintaxis
+//getline(cin,nombre);
+		cout << "Citas dentales Caro" << endl;
+		cout << "1.-Agendar cita\n2.-Modificar cita\n3.-Elminar cita\n4.-Lista de citas \n5.-Limpiar pantalla\n6.-Salir\n";
+		cout << "Ingrese la opción que guste: ";
+		cin >> opcion;
+		switch (opcion) {
+		case 1:
+///CAMBIAR A DO WHILE O WHILE 
+			for (int i = 1; i < 4; i++) {
+				cout << "-PACIENTE " << i << "-\n";
+				cout << "Nombre del Paciente: ";
+				cin >> nompac[i];
+				cout << "Edad: ";
+				cin >> edad[i];
+				cout << "\n** Cita agendada" 
+                "te esperamos! **\n";
+			}
+			break;
+		case 2:
+
+			break;
+		case 3:
+
+			break;
+		case 4:
+
+			break;
+		case 5:
+			system("cls");
+			opcion = 0;
+			break;
+		case 6:
+			exit(1);
+			break;
+		default:
+			cout << "\nIngrese una opción que sea válida\n";
+		}
+		if (opcion !=0 ) {
+			cout << "¿Quisiera volver al menú?(Sí=1 No=2)\nOpción: ";
+			cin >> salir;
+		}
+	} while (salir == 1);
+}
