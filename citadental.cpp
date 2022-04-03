aef53ba0eca895f3c32f5be3f4b27aeb1a9b3df3
carolinarecio <100321415+carolinarecio@users.noreply.github.com>
2022-04-03 17:46:51 -0500

    Update citadental.cpp

--- citadental.cpp
+++ citadental.cpp
@@ -1,51 +1,108 @@
 #include <iostream>
-
 using namespace std;
+
+struct info
+{
+    int id;
+    char nomp[50],nomt[50], desc[50];
+    float cant,hora, preut, precun;
+};
+
 int main()
 {
-	int opcion,edad[3];
-	int salir=1; 
-	char nompac[30], nomtrat[30], desc[100];
-	do {
+    info citas[10];
+    int opcion;
+    int menu;
+    int i;
+    int j,op;
+    do
+    {
+        cout << "\t" << "BIENVENIDO A CITA DENTAL CARO, ESCOJA LA OPCION QUE QUIERA" << "\n";
+        cout << "1. Agendar cita" << endl << "2. Modificar cita" << endl << "3. Eliminar cita" << endl << "4. Lista de citas vigentes" << endl << "5. Limpiar pantalla" << endl << "6. Salir" << endl;
+        cin >> opcion;
 
-		cout << "Citas dentales Caro" << endl;
-		cout << "1.-Agendar cita\n2.-Modificar cita\n3.-Elminar cita\n4.-Lista de citas \n5.-Limpiar pantalla\n6.-Salir\n";
-		cout << "Ingrese la opción que guste: ";
-		cin >> opcion;
-		switch (opcion) {
-		case 1:
-			while (int i = 1; i < 4; i++) {
-				cout << "-PACIENTE " << i << "-\n";
-				cout << "Nombre del Paciente: ";
-				cin >> nompac[i];
-				cout << "Edad: ";
-				cin >> edad[i];
-				cout << "\n** Cita agendada" 
-                "te esperamos! **\n";
-			}
-			break;
-		case 2:
+        switch (opcion)
+        {
+        case 1:
+             do{
+                 j = 1;
+            for (i = 0; i < j; i++)
+            {  
+                    cout << "Numero de su cita:" << i + 1 << "\n";
+                    cout << "Nombre del paciente que sera consultado:";
+                    cin >> citas[i].nomp;
+                    cout << "Hora de la cita:" << endl;
+                    cin >> citas[i].hora;
+                    cout << "Nombre del tratamiento:" << endl;
+                    cin >> citas[i].nomt;
+                    cout << "Descripcion del tratamiento:" << endl;
+                    cin >> citas[i].desc;
+                    cout << "Cantidad del tratamiento:" << endl;
+                    cin >> citas[i].cant;
+                    cout << "Precio:" << endl;
+                    cin >> citas[i].precun;
+                    cout << "¿Desea agendar otra cita? 1. si 2. no \n";
+                    cin >> op;
+                    if (op == 1) {
+                        j = j + 1;
+                    } 
+                    else {
+                        menu = 1;
+                    }
+                }; 
+                } while (op == 1);
+            break;
 
-			break;
-		case 3:
+        case 2:
+            cout << "¿Cual es id de la cita que quiera cambiar?" << endl;
+            cin >> j;
+            j = j - 1;
+            for (i = j; i <= j; i++)
+            {
+                cout << "El id es:" << i + 1 << "\n";
+                    cout << "Inserte nombre del paciente:" << "\n";
+                    cin >> citas[i].nomp;
+            }
+            break;
 
-			break;
-		case 4:
+        case 3:
+            break;
 
-			break;
-		case 5:
-			system("cls");
-			opcion = 0;
-			break;
-		case 6:
-			exit(1);
-			break;
-		default:
-			cout << "\nIngrese una opción que sea válida\n";
-		}
-		if (opcion !=0 ) {
-			cout << "¿Quisiera volver al menú?(Sí=1 No=2)\nOpción: ";
-			cin >> salir;
-		}
-	} while (salir == 1);
+        case 4:
+            for (i = 0; i <= j; i++)
+            {
+                cout << "el id es:" << i + 1 << "\n";
+                cout << "Nombre del paciente:";
+                cout << citas[i].nomp << "\n";
+                cout << "Hora del tratamiento:";
+                cout << citas[i].hora << "\n";
+                cout << "Nombre de su tratamiento:";
+                cout << citas[i].nomt << "\n";
+                cout << "Descripcion:";
+                cout << citas[i].desc << "\n";
+                cout << "Cantidad del tratamiento:";
+                cout << citas[i].cant << "\n";
+                cout << "Precio:" << endl;
+                cout << citas[i].precun << "\n";
+            }
+            menu = 1;
+            break;
+
+        case 5:
+
+            break;
+
+        case 6:
+            cout << "Saliendo del programa";
+            menu = 2;
+            break;
+
+        default:
+            cout << "su opcion es incorrecta";
+            menu = 1;
+            break;
+        }
+    } while (menu == 1);
+
+    return 0;
 }
